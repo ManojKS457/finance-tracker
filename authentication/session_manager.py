6213b229feb6af54e35fb587b7326b03de46e6f1
ManojKS457 <manojdab10@gmail.com>
2026-05-25 23:49:28 +0530

    Update session_manager.py

--- authentication/session_manager.py
+++ authentication/session_manager.py
@@ -1,7 +1,13 @@
 import streamlit as st
 
+
 def initialize_session():
 
     if "logged_in" not in st.session_state:
-
         st.session_state["logged_in"] = False
+
+    if "user_name" not in st.session_state:
+        st.session_state["user_name"] = ""
+
+    if "user_email" not in st.session_state:
+        st.session_state["user_email"] = ""
