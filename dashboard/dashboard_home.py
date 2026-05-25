2724ea8e542322d61739a8c41a5155475e839ab8
ManojKS457 <manojdab10@gmail.com>
2026-05-25 23:58:28 +0530

    Update dashboard_home.py

--- dashboard/dashboard_home.py
+++ dashboard/dashboard_home.py
@@ -1,49 +1,9 @@
 import streamlit as st
-import pandas as pd
 
 def show_dashboard():
 
-    st.header("📊 Dashboard")
+    st.title("📊 Dashboard")
 
-    try:
+    st.success("Dashboard Loaded Successfully")
 
-        df = pd.read_csv(
-            "dataset/personal_finance_dataset.csv"
-        )
-
-    except Exception as e:
-
-        st.error(f"Dataset Error: {e}")
-
-        return
-
-    income_df = df[df["type"] == "Income"]
-
-    expense_df = df[df["type"] == "Expense"]
-
-    total_income = income_df["amount"].sum()
-
-    total_expense = expense_df["amount"].sum()
-
-    savings = total_income - total_expense
-
-    col1, col2, col3 = st.columns(3)
-
-    col1.metric(
-        "💵 Total Income",
-        f"₹ {total_income:,.2f}"
-    )
-
-    col2.metric(
-        "💸 Total Expense",
-        f"₹ {total_expense:,.2f}"
-    )
-
-    col3.metric(
-        "💰 Savings",
-        f"₹ {savings:,.2f}"
-    )
-
-    st.subheader("Recent Transactions")
-
-    st.dataframe(df.tail(10))
+    st.write("Google Login Working ✅")
